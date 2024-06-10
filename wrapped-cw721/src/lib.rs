627cb8696b80d6a266aad99cd389b175c26f6e71
1pass-fi <charlesward19744@gmail.com>
2024-06-10 16:36:03 +0000

    initial commit

--- wrapped-cw721/src/lib.rs
+++ wrapped-cw721/src/lib.rs
@@ -1,7 +1,9 @@
 pub mod contract;
-mod error;
-pub mod helpers;
 pub mod msg;
 pub mod state;
 
-pub use crate::error::ContractError;
+// #[cfg(test)]
+// pub mod testing;
+
+#[cfg(all(not(target_arch = "wasm32"), feature = "testing"))]
+pub mod mock;
